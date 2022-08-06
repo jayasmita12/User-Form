8e75fd924483f137bdaae1fe1d10f304d88dea11
Jayasmita Sahu <96156277+jayasmita12@users.noreply.github.com>
2022-08-06 17:50:02 +0530

    frontend move into backend

--- Backend/index.js
+++ Backend/index.js
@@ -13,5 +13,5 @@ app.use(express.json())
 connectDB()
 app.use(bodyparser.urlencoded({extended:true}))
 app.use("/",router)
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
 app.listen(PORT,console.log(`Server running in PORT ${PORT}`))
